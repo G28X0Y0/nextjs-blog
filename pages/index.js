1b07a4409b123e8ff840d0e2ee2b0d117d7047bd
isaac <isaacmlopez@hotmail.com>
2022-10-25 13:47:09 -0400

    WIP stuff and HOLY CRAP NEXT 13 IS OUT (upgraded that ish)

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -14,6 +16,10 @@ export default function Home() {
             Feel free to reach out on {' '}
             <a href="https://www.linkedin.com/in/isaac-lopez-a67151172/">LinkedIn</a>
           </p>
+          <div id='blog'></div>
+          <h2>Blog</h2>
+          <Link href="/posts/first-post">First Post</Link>
+
         </section>
       </Layout>
   );
